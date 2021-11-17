11e25e4734d4608564994d16de9bf1a7eb0bbb82
Sergio Guillen <serguipe28@gmail.com>
2021-11-17 12:32:04 +0100

    Añadiendo cambios

--- addons/Competicion/models.py
+++ addons/Competicion/models.py
@@ -14,30 +14,42 @@ class propietario  (models.Model):
     
     name = fields.Char('Nombre', required=True)
     lastname = fields.Char('Apellido', required=True)
-    age = fields.Date(string='Edad', required=True)
+    age = fields.Date(string='Edad')
 
     #Relaciones entre tablas 
     dog_ids  = fields.One2many('competicion.dog', 'propietario_id')
     
     
 
+class participante (models.Model):
+    _name = 'competicion.participante'
+    _inherit = 'competicion.propietario'
+    _description = "Permite añadir informacion de agility a los propietarios"
+
+
+    licencia = fields.Integer(string="Numero de licencia", required=True)
+
+
+    #Relaciones entre tablas
+    dog_id = fields.Many2many('competicion.dog')
+
+
 
     
 class dog (models.Model):
     _name = 'competicion.dog'
     _description = 'Permite añadir un perro'
-    _order = 'licencia'
     
     name = fields.Char('Nombre del perro', required=True)
-    microchip = fields.Integer(string='Numero de microchip', required=True)
-    licencia = fields.Integer(string='Numero de licencia', required=True)  
+    microchip = fields.Integer(string='Numero de microchip', required=True)  
     fecha_nacimiento = fields.Date(string='Fecha de nacimiento', required=True)
     edad = fields.Integer(string='Edad del perro', compute = '_get_anos')
     raza = fields.Selection(string='Raza del perro', selection=[('b', 'Border Collie'),('s', 'Shetland'), ('c', 'Cocker')])
     
     #Relacion entre tablas
-    propietario_id = fields.Many2one('competicion.propietario', 'dog_ids') 
+    propietario_id = fields.Many2one('competicion.propietario', 'Propietario') 
     prueba_ids = fields.Many2many('competicion.prueba')
+    participante_id = fields.Many2many('competicion.participante')
     
     
     @api.depends('fecha_nacimiento')
@@ -61,3 +73,19 @@ class prueba  (models.Model):
     
     #Realacion entre tablas
     dog_ids = fields.Many2many('competicion.dog')    
+
+
+
+'''class LibraryBook(models.Model):
+ _name = 'library.book'
+ name = fields.Char('Title')
+ isbn = fields.Char('ISBN')
+ author_ids = fields.Many2many('res.partner', 'Authors')
+ @api.multi
+ def name_get(self):
+ result = []
+ for book in self:
+ authors = book.author_ids.mapped('name')
+ name = '%s (%s)' % (book.name, ', '.join(authors))
+ result.append((book.id, name))
+ return result>'''
